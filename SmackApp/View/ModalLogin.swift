4736e47f02cef03140a6549ff67e6428eeecdff2
August Danowski <august@thinkberkshire.com>
2017-09-27 12:34:13 -0400

    Added fields and styled modal view

--- SmackApp/View/ModalLogin.swift
+++ SmackApp/View/ModalLogin.swift
@@ -14,12 +14,18 @@ class ModalLogin: NSView {
 	
 	@IBOutlet weak var view: NSView!
 	
+	@IBOutlet weak var userNameText: NSTextField!
+	@IBOutlet weak var passwordText: NSSecureTextField!
+	@IBOutlet weak var loginButton: NSButton!
+	@IBOutlet weak var createAccountButton: NSButton!
+	@IBOutlet weak var stackView: NSStackView!
+	
+	
 	override init(frame frameRect: NSRect) {
 		super.init(frame: frameRect)
 		
 		Bundle.main.loadNibNamed(NSNib.Name(rawValue: "ModalLogin"), owner: self, topLevelObjects: nil)
 		
-		self.view.frame = NSRect(x: 0, y: 0, width: 475, height: 300)
 		addSubview(self.view)
 		
 	}
@@ -38,10 +44,31 @@ class ModalLogin: NSView {
 		setUpView()
     }
 	
+	@IBAction func closeModalClicked(_ sender: Any) {
+		
+	}
+	
+	@IBAction func loginButtonClicked(_ sender: Any) {
+		
+	}
+	
+	@IBAction func createAccountButtonClicked(_ sender: Any) {
+		
+	}
+	
 	func setUpView() {
 		
+		self.view.frame = NSRect(x: 0, y: 0, width: 475, height: 300)
+
 		view.layer?.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
 		view.layer?.cornerRadius = 7
+		
+		loginButton.layer?.backgroundColor = chatGreen.cgColor
+		loginButton.layer?.cornerRadius = 7
+		loginButton.styleButtonText(button: loginButton, buttonName: "Login", fontColor: .white, alignment: .center, font: AVENIR_BOLD, size: 14.0)
+		
+		createAccountButton.styleButtonText(button: createAccountButton, buttonName: "Create account", fontColor: chatGreen, alignment: .center, font: AVENIR_REGULAR, size: 12.0)
+		
 		
 	}
 }
